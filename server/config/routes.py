90da5213970be2ebbb35036662f27d2e031a25c6
Jimmy Gong <gong.jim@gmail.com>
2015-05-22 18:23:54 -0700

    Adds signup API to wardrobe

--- server/config/routes.py
+++ server/config/routes.py
@@ -1,6 +1,29 @@
 from flask import request, jsonify
+from ..db import db
+from ..models.users import User
+import json
 
 def router(app):
+  db.create_all()
+
   @app.route('/')
   def index():
     return app.send_static_file('index.html')
+
+  @app.route('/auth/signup', methods=['POST'])
+  def signup():
+    if (request.method == 'POST'):
+      data = json.loads(request.data)
+      
+      userObj = {
+        'isTaken': True
+      }
+
+      userExists = User.query.filter_by(username=data['username']).first()
+      if(not bool(userExists)):
+        user = User(data['username'], data['password'])
+        db.session.add(user)
+        db.session.commit()
+        userObj['isTaken'] = False
+
+      return jsonify(userObj)
